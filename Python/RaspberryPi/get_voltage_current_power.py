40c4efe67dce94d85ecad291827557c50a06fd88
David Hayden <dhayden@talosthoren.com>
2019-12-24 23:46:33 -0600

    Add logging.

--- Python/RaspberryPi/get_voltage_current_power.py
+++ Python/RaspberryPi/get_voltage_current_power.py
@@ -16,19 +16,21 @@ Copyright    GNU Lesser General Public License
 version  V1.0
 date  2019-2-27
 '''
-
 import time
+import logging
 from DFRobot_INA219 import INA219
 
 '''
 Revise the following two paramters according to actual reading of the INA219 and the multimeter
 for linearly calibration
 '''
+logging.basicConfig( filename="./testing.log" )
 
 ina219_reading_mA = 1000
 ext_meter_reading_mA = 1000
 
-ina = INA219(1, INA219.INA219_I2C_ADDRESS4)                                #Change I2C address by dialing DIP switch
+#Change I2C address by dialing DIP switch
+ina = INA219(1, INA219.INA219_I2C_ADDRESS4)                                
 
 #begin return True if succeed, otherwise return False
 while not ina.begin():
@@ -36,21 +38,15 @@ while not ina.begin():
 
 ina.linear_cal(ina219_reading_mA, ext_meter_reading_mA)
 
-
-#ina.reset()                                     #Resets all registers to default values
-
 def main():
+    logger = logging.getLogger('ina219_sensor')
+    logger.setLevel( logging.INFO )
+
     while True:
         time.sleep(1)
-        '''
-        print ("Shunt Voltage : %.2f mV" % ina.get_shunt_voltage_mV())
-        print ("Bus Voltage   : %.3f V" % ina.get_bus_voltage_V())
-        print ("Current       : %.f mA" % ina.get_current_mA())
-        print ("Power         : %.f mW" % ina.get_power_mW())
-        print (" ")
-        '''
-        print( _readSensorValues() )
-
+        sensorValues = _readSensorValues()
+        logger.info( "%s", sensorValues )
+        
 def _readSensorValues():
     sensorValues = {
         "ShuntVoltage": ina.get_shunt_voltage_mV(),
